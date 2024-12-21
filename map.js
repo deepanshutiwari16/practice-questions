51befa6fc085a3bbc6f357c579eecb4bfb8d9694
Deepanshu Tiwari <183710540+deepanshutiwari16@users.noreply.github.com>
2024-12-21 21:17:45 +0530

    added uppercaseOf

--- map.js
+++ map.js
@@ -15,7 +15,11 @@ const lengthsOf = function (strings) {
 // console.log(lengthsOf(["apple", "a", ""]));
 
 // uppercase of ["hello", "world"] => ["HELLO", "WORLD"]
-const uppercaseOf = function (strings) { };
+const uppercaseOf = function (strings) {
+  return strings.map(function (string) {return string.toUpperCase()})
+};
+
+// console.log(uppercaseOf(["hello", "world"]));
 
 // first characters of ["apple", "banana", "kiwi"] => ["a", "b", "k"]
 const firstCharactersOf = function (strings) { };
